e6a93b8db08deee92522ced53581c53a0b86a747
sargeliu <53758352+Sargeliu@users.noreply.github.com>
2023-05-08 15:04:44 +0500

    Логика второго экрана. Добавляем ссылки на все элементы и проинициализируем в методе initViews(). Также ключ выносим в константу.

--- app/src/main/java/com/example/cafe/MakeOrderActivity.java
+++ app/src/main/java/com/example/cafe/MakeOrderActivity.java
@@ -5,25 +5,56 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MakeOrderActivity extends AppCompatActivity {
+
+    private static final String EXTRA_USER_NAME = "userName";
+    private TextView textViewGreetings;
+    private RadioGroup radioGroupDrinks;
+
+    private RadioButton radioButtonTea;
+    private RadioButton radioButtonCoffee;
+    private TextView textViewAdditives;
+    private CheckBox checkBoxSugar;
+    private CheckBox checkBoxMilk;
+    private CheckBox checkBoxLemon;
+    private Spinner spinnerCoffee;
+    private Spinner spinnerTea;
+    private Button buttonMakeOrder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_order);
+        initViews();
 
-        TextView textViewGreetings = findViewById(R.id.textViewGreetings);
-
-        Intent intent = getIntent();
-        String userName = intent.getStringExtra("userName");
+        String userName = getIntent().getStringExtra(EXTRA_USER_NAME);
         textViewGreetings.setText(userName);
     }
 
     public static Intent newIntent(Context context, String userName) {
         Intent intent = new Intent(context, MakeOrderActivity.class);
-        intent.putExtra("userName", userName);
+        intent.putExtra(EXTRA_USER_NAME, userName);
         return intent;
+    }
+
+    private void initViews() {
+        textViewGreetings = findViewById(R.id.textViewGreetings);
+        radioGroupDrinks  = findViewById(R.id.radioGroupDrinks);
+        radioButtonTea  = findViewById(R.id.radioButtonTea);
+        radioButtonCoffee  = findViewById(R.id.radioButtonCoffee);
+        textViewAdditives  = findViewById(R.id.textViewAdditives);
+        checkBoxSugar  = findViewById(R.id.checkBoxSugar);
+        checkBoxMilk  = findViewById(R.id.checkBoxMilk);
+        checkBoxLemon  = findViewById(R.id.checkBoxLemon);
+        spinnerCoffee  = findViewById(R.id.spinnerCoffee);
+        spinnerTea  = findViewById(R.id.spinnerTea);
+        buttonMakeOrder  = findViewById(R.id.buttonMakeOrder);
     }
 }
